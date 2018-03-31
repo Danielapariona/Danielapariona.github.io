4d005649ece6dd25c7a3b8ea8d7715551c181a85
Danielapariona <daniela.parionac@gmail.com>
2018-03-31 16:57:27 -0500

    Add logos

--- js/app.js
+++ js/app.js
@@ -1,21 +1,28 @@
 $(document)
-.ready(function() {
-  // fix menu when passed
-  $('.masthead')
-    .visibility({
-      once: false,
-      onBottomPassed: function() {
-        $('.fixed.menu').transition('fade in');
-      },
-      onBottomPassedReverse: function() {
-        $('.fixed.menu').transition('fade out');
-      }
-    })
-  ;
+  .ready(function () {
+    // fix menu when passed
+    $('.masthead')
+      .visibility({
+        once: false,
+        onBottomPassed: function () {
+          $('.fixed.menu').transition('fade in');
+        },
+        onBottomPassedReverse: function () {
+          $('.fixed.menu').transition('fade out');
+        }
+      });
 
-  // create sidebar and attach to menu open
-  $('.ui.sidebar')
-    .sidebar('attach events', '.toc.item')
-  ;
-})
-;
+    // create sidebar and attach to menu open
+    $('.ui.sidebar')
+      .sidebar('attach events', '.toc.item');
+
+    var $grayscale = $('.grayscale');
+    $($grayscale).hover(
+      function () {
+        $(this).addClass('gray')
+      },
+      function () {
+        $(this).removeClass('gray')
+      }
+    )
+  });
